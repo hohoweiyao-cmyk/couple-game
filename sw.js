00db1b062d508f4b056832eba7e69f99397f46ad
hohoweiyao-cmyk <hohoweiyao@gmail.com>
2026-07-07 21:03:36 +0800

    更新升温牌库与移动端体验

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'couple-game-v4';
+var CACHE = 'couple-game-v5';
 var ASSETS = ['./','./index.html','./manifest.json'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}));
